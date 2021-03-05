64bdb9e5e4b5520699ca5a92cea3ef276f3a984f
majazocom <maja.gedda@zocom.se>
2021-03-05 10:25:15 +0100

    push the error

---
+++ src/components/PokemonCard.vue
@@ -0,0 +1,18 @@
+<template>
+  <section class="card">
+      <h2>{{ this.pokemon.name }}</h2>
+  </section>
+</template>
+
+<script>
+export default {
+    name: 'PokemonCard',
+    props: {
+        pokemon: Object
+    }
+}
+</script>
+
+<style>
+
+</style>
